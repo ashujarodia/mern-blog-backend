42652704c2d44794ef6bb04a58fdac03f6083bea
ashujarodia <112541439+ashujarodia@users.noreply.github.com>
2024-02-12 16:03:42 +0530

    commited

--- data/database.js
+++ data/database.js
@@ -4,6 +4,6 @@ export const connectDB = () => {
 	mongoose.connect(process.env.MONGO_URI, {
 		dbName: 'blog-mern',
 	})
-		.then(() => console.log('Database connected'))
+		.then(() => console.log('Database connected '))
 		.catch((error) => console.log(error));
 };
